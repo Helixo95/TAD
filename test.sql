089a395cfb5a7fd3cdacfc54ce0771654c6ef0cf
Aurélien <aurel.ruppe@gmail.com>
2025-04-01 22:14:17 +0200

    add del tables

--- test.sql
+++ test.sql
@@ -21,3 +21,19 @@ ORDER BY column_id;
 
 
 DESCRIBE ORDINATEURS;
+
+SELECT name FROM v$database;
+
+
+BEGIN
+    -- Boucle sur toutes les tables de bdd_opti et suppression
+    FOR t IN (SELECT table_name
+              FROM all_tables
+              WHERE owner = 'C##NEW_SYS'  -- C##NEW_SYS est l'utilisateur propriétaire de bdd_opti
+                AND table_name NOT LIKE 'BIN$%' -- Optionnel : Exclut les tables système
+    ) LOOP
+        -- Exécuter la suppression de chaque table dans bdd_opti
+        EXECUTE IMMEDIATE 'DROP TABLE C##NEW_SYS.' || t.table_name || ' CASCADE';
+    END LOOP;
+END;
+/
