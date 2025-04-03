6eba420916fdd7c37949f06cd02a439d9caa7680
Aurélien <aurel.ruppe@gmail.com>
2025-04-03 18:58:30 +0200

    add rand for ticket status

--- tests/index_origin.sql
+++ tests/index_origin.sql
@@ -16,7 +16,7 @@ DECLARE
     l_count INT := 0;  -- Déclaration du compteur
     v_email1 VARCHAR2(50) := 'sarah.lemoine@example.com'; -- Changer l'email si besoin
     v_email2 VARCHAR2(50) := 'email1000@example.com'; -- Changer l'email si besoin
-    v_statut INT := 2; -- Statut "résolu"
+    v_statut INT;
     
     -- Définir le type de table pour collecter plusieurs tickets dans le schéma 'C##ADMIN_SYS_ORIGIN'
     TYPE ticket_table_type IS TABLE OF C##ADMIN_SYS_ORIGIN.tickets%ROWTYPE;
@@ -26,6 +26,9 @@ DECLARE
 
     v_count INTEGER := 0; -- Compteur pour les tickets
 BEGIN
+    -- Initialisation du générateur de nombres aléatoires
+    DBMS_RANDOM.SEED(TO_CHAR(SYSTIMESTAMP, 'YYYYMMDDHH24MISSFF'));
+
     -- Temps de début
     v_start := SYSTIMESTAMP;
 
@@ -38,12 +41,15 @@ BEGIN
             v_lieu := 'Pau';
         END IF;
 
+        -- Générer un statut aléatoire entre 0 et 3
+        v_statut := TRUNC(DBMS_RANDOM.VALUE(0, 4));
+
         -- Ajout d'un élève avec des tickets et logs
         INSERT INTO C##ADMIN_SYS_ORIGIN.eleves (eleve_id, nom, prenom, email, password, classe, specialite, filiere, lieu)
         VALUES (90 + i, 'Nom' || i, 'Prénom' || i, 'email' || i || '@example.com', 'pass' || i, 'Classe' || i, 'Spécialité' || i, 'Filière' || i, v_lieu);
 
         INSERT INTO C##ADMIN_SYS_ORIGIN.tickets (ticket_id, sujet, description, statut, date_ouverture, date_fermeture, eleve_id, assigne_id)
-        VALUES (i + 1000, 'Problème ' || i, 'Problème numéro ' || i, 0, CURRENT_TIMESTAMP, NULL, 90 + i, 1);
+        VALUES (i + 1000, 'Problème ' || i, 'Problème numéro ' || i, v_statut, CURRENT_TIMESTAMP, NULL, 90 + i, 1);
 
         INSERT INTO C##ADMIN_SYS_ORIGIN.logs (log_id, action, date_action, eleve_id, ticket_id)
         VALUES (i + 2000, 'Création du ticket', CURRENT_TIMESTAMP, 90 + i, i + 1000);
@@ -64,10 +70,10 @@ BEGIN
     v_diff := v_end - v_start;
 
     -- Afficher le temps d'exécution
-    DBMS_OUTPUT.PUT_LINE('Temps d exécution : ' || v_diff);
+    DBMS_OUTPUT.PUT_LINE('Temps d''exécution : ' || v_diff);
 
     -- Afficher le nombre d'élèves insérés
-    DBMS_OUTPUT.PUT_LINE('Nombre d élèves insérés : ' || l_count);
+    DBMS_OUTPUT.PUT_LINE('Nombre d''élèves insérés : ' || l_count);
 
 --------------------------------------------------------------------------
     -- Sélection d'un élève par son email
